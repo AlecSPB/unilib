199153cfb772835e5b16343cc1acc8f39f04cd46
hyoguen.park <hyogeun.park@12corp.com>
2015-10-16 15:04:03 +0900

    [박효근] 슬라이드 탭 추가

--- app/src/main/java/com/seoulapp/unilib/MainActivity.java
+++ app/src/main/java/com/seoulapp/unilib/MainActivity.java
@@ -1,19 +1,51 @@
 package com.seoulapp.unilib;
 
-import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+
+import com.seoulapp.unilib.views.SlidingTabLayout;
 
 
 public class MainActivity extends BaseAppCompatActivity {
-
+    private ViewPager mPager;
+    private ViewPagerAdapter mPagerAdapter;
+    private SlidingTabLayout mSlidingTabLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitleText("능동");
+        mPager = (ViewPager) findViewById(R.id.fragment_container);
+        mPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        mPager.setAdapter(mPagerAdapter);
+        mSlidingTabLayout = (SlidingTabLayout) findViewById(R.id.header);
+        mSlidingTabLayout.setSelectedIndicatorColors(getResources().getColor(R.color.br_blue));
+        mSlidingTabLayout.setDistributeEvenly(true);
+        mSlidingTabLayout.setViewPager(mPager);
+    }
+
+    private class ViewPagerAdapter extends FragmentPagerAdapter{
+
+        private String[] mPageTitle = {"BOOK SHARING", "MY PAGE"};
+        public ViewPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            return new Fragment();
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mPageTitle[position];
+        }
     }
 }
