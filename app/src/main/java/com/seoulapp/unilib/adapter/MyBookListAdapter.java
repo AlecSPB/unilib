fe4e588d4bc9ee58527c5deea6985f560466e422
hyoguen.park <hyogeun.park@12corp.com>
2015-10-22 10:19:45 +0900

    [박효근] my page 추가

---
+++ app/src/main/java/com/seoulapp/unilib/adapter/MyBookListAdapter.java
@@ -0,0 +1,42 @@
+package com.seoulapp.unilib.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import com.seoulapp.unilib.R;
+
+/**
+ * Created by hyoguen.park on 2015-10-22.
+ */
+public class MyBookListAdapter extends BaseAdapter {
+    private Context mContext;
+    public MyBookListAdapter(Context context) {
+        mContext = context;
+    }
+    @Override
+    public int getCount() {
+        return 10;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return position;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if(convertView != null) {
+            LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.my_book_list_item, parent, false);
+        }
+        return null;
+    }
+}
--- app/src/main/java/com/seoulapp/unilib/mypage/MyPageFragment.java
+++ app/src/main/java/com/seoulapp/unilib/mypage/MyPageFragment.java
@@ -9,19 +9,23 @@ import android.view.ViewGroup;
 import android.widget.ListView;
 
 import com.seoulapp.unilib.R;
+import com.seoulapp.unilib.adapter.MyBookListAdapter;
 
 /**
  * Created by SAMSUNG on 2015-10-18.
  */
 public class MyPageFragment extends Fragment {
     private ListView mListView;
+    private MyBookListAdapter mListAdapter;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_my_page,container,false);
         mListView = (ListView)rootView.findViewById(R.id.my_page_list_view);
+        mListAdapter = new MyBookListAdapter(getActivity());
         View headerView = inflater.inflate(R.layout.header_my_page, null, false);
         mListView.addHeaderView(headerView);
+        mListView.setAdapter(mListAdapter);
         return rootView;
     }
 }
